6009ce9590d1d7fded2d59be7c4218a082354dab
delftin <resendo13@gmail.com>
2025-03-19 11:22:14 +0900

    로그인 페이지 스타일

--- everycar-frontend/src/routes/authorization/Login.js
+++ everycar-frontend/src/routes/authorization/Login.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from "../../css/routes/authorization/Login.module.scss";
 
 import useLogin from '../../components/hooks/useLogin';
 
 const Login = () => {
+  const navigate = useNavigate();
+
   const { login, errorMessage } = useLogin(); // 커스텀 훅 사용
   const [userId, setUserId] = useState('');
   const [userPassword, setUserPassword] = useState('');
@@ -14,33 +17,46 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>로그인</h1>
+    <div className={styles.login}>
+      <h1 className={styles.title}>로그인</h1>
+
+      {errorMessage && <p style={{ color: 'red' }} className={styles.errorMessage}>{errorMessage}</p>}
+
       <form onSubmit={handleSubmit}>
-        <label htmlFor="userId">아이디:</label>
-        <input
-          type="text"
-          id="userId"
-          name="userId"
-          value={userId}
-          onChange={(e) => setUserId(e.target.value)}
-          required
-        /><br />
+        <div className={styles.loginInput}>
+          <label htmlFor="userId" className={styles.subTitle}>아 이 디: </label>
+          <input
+            type="text"
+            id="userId"
+            name="userId"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+            required
+          /><br />
+        </div>
 
-        <label htmlFor="userPassword">비밀번호:</label>
-        <input
-          type="password"
-          id="userPassword"
-          name="userPassword"
-          value={userPassword}
-          onChange={(e) => setUserPassword(e.target.value)}
-          required
-        /><br />
+        <div className={styles.loginInput}>
+          <label htmlFor="userPassword" className={styles.subTitle}>비밀번호: </label>
+          <input
+            type="password"
+            id="userPassword"
+            name="userPassword"
+            value={userPassword}
+            onChange={(e) => setUserPassword(e.target.value)}
+            required
+          /><br />
+        </div>
 
-        <button type="submit">로그인</button>
+        <div className={styles.buttonContainer}>
+          <button type="submit" className={styles.loginButton}>로그인</button>
+          <button
+            className={styles.registerButton}
+            onClick={() => navigate('/auth/register') }
+          >
+            회원가입
+          </button>
+        </div>
       </form>
-
-      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
     </div>
   );
 };
